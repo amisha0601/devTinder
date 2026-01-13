72d77a64145d7cb8107d0b7e44e9c17c5ac6006b
Amisha Singh <amishaaworks06@gmail.com>
2026-01-13 22:46:51 +0530

    Explore GET, POST, DELETE APIs

--- src/app.js
+++ src/app.js
@@ -2,17 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req,res) => {
-    res.send("Hello hello hello hello!");
+app.use("/user",(req,res) => {
+   res.send("HAHHAHAHAHAHAHA")
+})
+
+//This will only handle GET call to /user
+app.get("/user", (req,res) => {
+    res.send({firstName: "Amisha", lastName: "Singh"});
 });
+
+app.post("/user",(req,res) => {
+   //saving data to DB
+   res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user",(req,res) => {
+    res.send("Deleted successfully!")
+})
+
+//This will match all the HTTP method API calls to /test
 app.use("/test", (req,res) => {
     res.send("Hello from the server!");
 });
 
-app.use("/", (req,res) => {
-    res.send("Hello from the dashboard!");
-});
-
-app.listen(7000, () => {
-    console.log("Server is successfully running on port 7000...");
+app.listen(8080, () => {
+    console.log("Server is successfully running on port 8080...");
 });

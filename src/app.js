dfee2a60bfb45a48fa7c43002eb6b6160972c19a
Amisha Singh <amishaaworks06@gmail.com>
2026-01-16 02:03:59 +0530

    Create POST/ signup API to add dummy user data document  into database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
+app.post("/signup", async (req,res) => {
+   const user = new User({
+    firstName: "Anushka",
+    lastName: "Singh",
+    emailId: "anushk@singh.com",
+    password: "anushka@123"
+   });
 
+   try {await user.save();
+   res.send("User Added Successfully!!");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 
 connectDB().then(() => {
   console.log("Database connection established...");

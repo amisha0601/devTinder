ae25eda4c52ec13025d64ad637a5cdcc99091040
Amisha Singh <amishaaworks06@gmail.com>
2026-01-18 01:41:04 +0530

    add GET /user and /feed APIs

--- src/app.js
+++ src/app.js
@@ -5,22 +5,54 @@ const User = require("./models/user");
 
 app.use(express.json());
 
-app.post("/signup", async (req,res) => {
-   const user = new User(req.body);
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
 
-   try {await user.save();
-   res.send("User Added Successfully!!");
+  try {
+    await user.save();
+    res.send("User Added Successfully!!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
   }
 });
 
-connectDB().then(() => {
-  console.log("Database connection established...");
-  app.listen(8080, () => {
-    console.log("Server is successfully running on port 8080...");
-});
-}).catch(err => {
-   console.error("Database cannot be connected!!")
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(user);
+    }
+
+    //  const users =  await User.find({emailId: userEmail});
+    //   if(users.length === 0){
+    //     res.status(404).send("User not found");
+    //   } else{
+    //     res.send(users);
+    //   }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
 });
 
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(8080, () => {
+      console.log("Server is successfully running on port 8080...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });

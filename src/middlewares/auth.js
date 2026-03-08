fe37a0774f626b30db1c75bcafa4975e1dc7857b
Amisha Singh <amishaaworks06@gmail.com>
2026-03-08 13:29:32 +0530

    add connection request API with validations and duplicate check

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,23 +5,27 @@ const userAuth = async (req, res, next) => {
   try {
     const cookies = req.cookies;
     const { token } = cookies;
-    if(!token) {
-    return res.status(401).send("Token is not there. Please Login!");
-}
-    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+
+    if (!token) {
+      return res.status(401).send("Token is not there. Please Login!");
+    }
+
+    const decodedObj = jwt.verify(token, "DEV@Tinder$790");
 
     const { _id } = decodedObj;
     const user = await User.findById(_id);
+
     if (!user) {
       throw new Error("User not found");
     }
 
     req.user = user;
+
     next();
   } catch (err) {
+    console.log("AUTH ERROR =>", err);
     res.status(400).send("ERROR: " + err.message);
   }
 };
-module.exports = {
-  userAuth,
-};
+
+module.exports = userAuth;
--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,8 @@ const userSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
-      minLength: 6,
+      minLength: 4,
+      maxLength: 50,
     },
     lastName: {
       type: String,
@@ -40,16 +41,19 @@ const userSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data is not valid");
-        }
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
       },
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data is not valid");
+      //   }
+      // },
     },
     photoUrl: {
       type: String,
-      default:
-        "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png",
+      default: "https://geographyandyou.com/images/user-profile.png",
       validate(value) {
         if (!validator.isURL(value)) {
           throw new Error("Invalid Photo URL: " + value);
@@ -71,22 +75,24 @@ const userSchema = new mongoose.Schema(
 
 userSchema.methods.getJWT = async function () {
   const user = this;
+
   const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
     expiresIn: "7d",
   });
+
   return token;
 };
 
 userSchema.methods.validatePassword = async function (passwordInputByUser) {
   const user = this;
   const passwordHash = user.password;
+
   const isPasswordValid = await bcrypt.compare(
     passwordInputByUser,
     passwordHash,
   );
+
   return isPasswordValid;
 };
 
-const userModel = mongoose.model("User", userSchema);
-
-module.exports = userModel;
+module.exports = mongoose.model("User", userSchema);
